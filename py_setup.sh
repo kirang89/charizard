b54eafc83228deab8fdd943efa99a285d9131b49
kirang89 <kiran.daredevil@gmail.com>
2013-04-01 11:11:02 +0530

    added fabric and sphinx installs

--- py_setup.sh
+++ py_setup.sh
@@ -2,10 +2,13 @@
 # Additional setup 
 #
 
+#Pip modules
 sudo pip install virtualenv
 sudo pip install sqlalchemy
-sudo pip install fabric
-sudo pip install django
 sudo pip install flask
 sudo pip install simplejson
 
+#Installing other modules via CL
+sudo apt-get install ipython ipython-notebook
+sudo apt-get install fabric
+sudo apt-get install python-sphinx
